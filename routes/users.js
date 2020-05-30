d3316b55ad65bd0a38edadd6922a003f2c85a2ce
Daniel Ong <danielonges@Daniels-MacBook-Pro-2.local>
2020-05-30 20:55:41 +0800

    Forgot to save

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
