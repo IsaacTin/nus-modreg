675bb3e5c6391a3660126a55b02eb7083e7f048a
Isaac <tin.isaac@yahoo.com.sg>
2020-07-26 03:18:23 +0800

    updated map

--- client/src/components/pages/Location.js
+++ client/src/components/pages/Location.js
@@ -5,17 +5,41 @@ import MainMap from '../map/MainMap';
 import MapMenu from '../map/MapMenu';
 import MapTable from '../map/MapTable';
 import MapContext from '../../context/map/mapContext';
+import ModuleContext from '../../context/module/moduleContext';
+import coordsArrayConverter from  '../../utils/coordsArrayConverter';
+
 
 const Location = () => {
     const layoutContext = useContext(LayoutContext);
     const { isSidebarOpen } = layoutContext;
 
-    const {getBusStops, } = useContext(MapContext);
+    const {getBusStops, setCoords, setDay, day, currentCoords} = useContext(MapContext);
+    const {displayedModules} = useContext(ModuleContext)
 
     useEffect(() => {
         getBusStops();
     // eslint-disable-next-line
     }, [])
+
+    useEffect(() => {
+        if (displayedModules.length !== 0 && day === null && currentCoords.length === 0) {
+            setDay("Monday");
+            let temp = [];
+            displayedModules.forEach((module) => {
+                if (
+                    module.timing.filter((timeslot) => timeslot.day === day)
+                        .length !== 0
+                ) {
+                    temp.push(module);
+                }
+            });
+            console.log(temp);
+            const fetchCoords = async () => {
+                setCoords(await coordsArrayConverter(temp, day));
+            };
+            fetchCoords();
+        }
+    },[])
 
 
     return (
